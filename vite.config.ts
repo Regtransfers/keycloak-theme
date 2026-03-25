3cd337abc25b35fc5389c3844542b0491cadf97b
rtrossm <ross@regtransfers.co.uk>
2026-03-25 15:07:33 +0000

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import { keycloakify } from "keycloakify/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         react(),
         keycloakify({
             accountThemeImplementation: "none",
@@ -13,5 +16,10 @@ export default defineConfig({
             // you can select either "keycloak-theme" or "keycloak-theme-dark".
             themeName: ["keycloak-theme", "keycloak-theme-dark"]
         })
-    ]
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src")
+        }
+    }
 });
