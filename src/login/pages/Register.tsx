3c24d7190428b5e376eba5f9539b9a82679a9caf
Aaron <aaron@regtrasnfers.co.uk>
2026-06-24 12:40:59 +0100

    Trim att down

--- src/login/pages/Register.tsx
+++ src/login/pages/Register.tsx
@@ -135,7 +135,7 @@ export default function Register({ kcContext, i18n }: Props) {
                 <div className="flex flex-col gap-2">
                     <input
                         type="hidden"
-                        name="user.attributes.marketingConsent"
+                        name="marketingConsent"
                         value={marketingConsent ? "true" : "false"}
                     />
                     <div className="flex items-start gap-3 py-3">
